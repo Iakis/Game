a1b1a92d99ca6d8348f5e324dc91715df7094c61
CYTHIX-PC\Cythix <nuvanacythix>
2017-03-18 02:16:25 -0400

    Some player controls finished

---
+++ PlayerControls/Assets/Scripts/PlayerDodging.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDodging : MonoBehaviour
+{
+
+	// Use this for initialization
+	public float timeLimit;
+	private float timeRemaining;
+	Vector3 temp;
+	void Start()
+	{
+		timeLimit = .5f;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		//Shift Dodging
+		if (Input.GetAxisRaw("Vertical") > 0 && Input.GetKeyDown(KeyCode.LeftShift))
+			transform.position += Vector3.forward * 5;
+		if (Input.GetAxisRaw("Vertical") < 0 && Input.GetKeyDown(KeyCode.LeftShift))
+			transform.position += Vector3.back * 5;
+		if (Input.GetAxisRaw("Horizontal") > 0 && Input.GetKeyDown(KeyCode.LeftShift))
+			transform.position += Vector3.right * 5;
+		if (Input.GetAxisRaw("Horizontal") < 0 && Input.GetKeyDown(KeyCode.LeftShift))
+			transform.position += Vector3.left * 5;
+
+		
+		
+		//Double tap dodging
+
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			timeRemaining = timeLimit;
+			if (canDodge())
+				if (Input.GetKeyDown(KeyCode.W))
+					transform.position += Vector3.forward * 5;
+		}
+		Debug.Log(canDodge());
+	}
+
+	bool canDodge()
+	{
+		if (timeRemaining > 0)
+		{
+			timeRemaining -= Time.deltaTime;
+			return true;
+		}
+		else if (timeRemaining <= 0)
+		{
+			timeRemaining = 0f;
+			return false;
+		}
+		return false;
+	}
+}
---
+++ PlayerControls/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float currentMoveSpeed;
+    private Transform trans;
+	// Use this for initialization
+	void Start () {
+        currentMoveSpeed = 1f;
+        trans = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Movement
+
+        if (Input.GetAxisRaw("Vertical") > 0)
+            transform.Translate(Vector3.forward * currentMoveSpeed * Time.deltaTime);
+        if (Input.GetAxisRaw("Vertical") < 0)
+            transform.Translate(-Vector3.forward * currentMoveSpeed * Time.deltaTime);
+        if (Input.GetAxisRaw("Horizontal") > 0)
+            transform.Translate(Vector3.right * currentMoveSpeed * Time.deltaTime);
+        if (Input.GetAxisRaw("Horizontal") < 0)
+            transform.Translate(Vector3.left * currentMoveSpeed * Time.deltaTime);
+    }
+}
