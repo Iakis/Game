2cbf046ff70c25496e7046e3cd38e225af172e32
CYTHIX-PC\Cythix <nuvanacythix>
2017-05-30 22:37:32 -0400

    Final

--- PlayerControls/Assets/Scripts/AI/MoveTowards.cs
+++ PlayerControls/Assets/Scripts/AI/MoveTowards.cs
@@ -9,13 +9,17 @@ public class MoveTowards : MonoBehaviour
 	public GameObject target;
 	public float speed;
 	private Collider col;
-	bool inputed;
-	public Text cont;
 
+	bool reset = false;
+
+	private int lastDirection = 0;
 	public GameMan gameManageScript;
 	public GameObject gameManageEmpty;
+
+	private Animator anim;
 	private void Start()
 	{
+		anim = GetComponentInChildren<Animator>();
 		col = target.GetComponent<Collider>();
 		gameManageEmpty = GameObject.FindGameObjectWithTag("GamManage");
 		gameManageScript = gameManageEmpty.GetComponent<GameMan>();
@@ -24,6 +28,31 @@ public class MoveTowards : MonoBehaviour
 	void Update()
 	{
 		float step = speed * Time.deltaTime;
+		Vector3 diff = target.transform.position - transform.position;
+		int dir = (int)(((Mathf.Atan2(diff.z, diff.x) * Mathf.Rad2Deg) + 135f) / 90);
+
+
+		if (reset)
+		{
+			anim.SetInteger("Direction", -1);
+			reset = false;
+		}
+
+
+		if (dir != lastDirection)
+		{
+			anim.SetInteger("Direction", dir);
+			lastDirection = dir;
+			reset = true;
+		}
+
+
+
+
+
+
+
+		//Debug.Log((int)(((Mathf.Atan2(diff.z, diff.x) * Mathf.Rad2Deg) + 135f) / 90));
 		transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
 	}
 	private void OnTriggerEnter(Collider other)
--- PlayerControls/Assets/Scripts/Player Controls/PlayerMovement.cs
+++ PlayerControls/Assets/Scripts/Player Controls/PlayerMovement.cs
@@ -6,34 +6,42 @@ public class PlayerMovement : MonoBehaviour
 {
 	public float baseMoveSpeed;
     public float currentMoveSpeed;
+	public Animator anim;
     // Use this for initialization
     void Start()
 	{
+		anim = GetComponentInChildren<Animator>();
 		baseMoveSpeed = 5f;
 		currentMoveSpeed = baseMoveSpeed;
-    }
+		anim.SetInteger("Direction", 0);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
+		anim.SetInteger("Direction", -1);
+
 		//Movement
-		
-		/*if (Input.GetAxisRaw("Vertical") > 0)
-            transform.Translate(Vector3.forward * currentMoveSpeed * Time.deltaTime);
-        if (Input.GetAxisRaw("Vertical") < 0)
-            transform.Translate(-Vector3.forward * currentMoveSpeed * Time.deltaTime);
-        if (Input.GetAxisRaw("Horizontal") > 0)
-            transform.Translate(Vector3.right * currentMoveSpeed * Time.deltaTime);
-        if (Input.GetAxisRaw("Horizontal") < 0)
-            transform.Translate(Vector3.left * currentMoveSpeed * Time.deltaTime);
-			*/
-		if (Input.GetKeyDown(KeyCode.W) || Input.GetKey(KeyCode.W))
+		if (Input.GetKey(KeyCode.W))
 			transform.Translate(Vector3.forward * currentMoveSpeed * Time.deltaTime);
-		if (Input.GetKeyDown(KeyCode.S) || Input.GetKey(KeyCode.S))
+		if (Input.GetKey(KeyCode.S))
 			transform.Translate(-Vector3.forward * currentMoveSpeed * Time.deltaTime);
-		if (Input.GetKeyDown(KeyCode.A) || Input.GetKey(KeyCode.A))
+		if (Input.GetKey(KeyCode.A))
 			transform.Translate(Vector3.left * currentMoveSpeed * Time.deltaTime);
-		if (Input.GetKeyDown(KeyCode.D) || Input.GetKey(KeyCode.D))
-			transform.Translate(Vector3.right * currentMoveSpeed * Time.deltaTime);
-    }
+		if (Input.GetKey(KeyCode.D))
+			transform.Translate(Vector3.right * currentMoveSpeed * Time.deltaTime);		
+
+
+		if (Input.GetKeyDown(KeyCode.W))
+			anim.SetInteger("Direction", 3);
+		if (Input.GetKeyDown(KeyCode.S))
+			anim.SetInteger("Direction", 4);
+		if (Input.GetKeyDown(KeyCode.A))
+			anim.SetInteger("Direction", 2);
+		if (Input.GetKeyDown(KeyCode.D))
+			anim.SetInteger("Direction", 1);
+
+		if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+			anim.SetInteger("Direction", 0);
+	}
 }
